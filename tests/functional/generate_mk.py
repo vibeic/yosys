4be1695d2dec62f3b54224000ffc87ea8faf7b46
Miodrag Milanovic <mmicko@gmail.com>
2026-07-03 07:54:10 +0200

    enable functional tests

--- tests/functional/generate_mk.py
+++ tests/functional/generate_mk.py
@@ -6,6 +6,6 @@ sys.path.append("..")
 import gen_tests_makefile
 
 def callback():
-    gen_tests_makefile.generate_cmd_test("functional",f'pytest -v -m "not smt and not rkt" . "$$@"')
+    gen_tests_makefile.generate_cmd_test("functional",f'pytest -n auto -v -m "not smt and not rkt" . "$$@"')
 
 gen_tests_makefile.generate_custom(callback)
